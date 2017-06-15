c04478bb1b3b9ac802b52a462cdc07452dab69fb
Ate <poortinga.ate@gmail.com>
2017-06-15 13:51:48 -0700

    new landuse map

--- server.py
+++ server.py
@@ -28,22 +28,27 @@ class MainPage(webapp2.RequestHandler):
     ee.Initialize(config.EE_CREDENTIALS)
 
     # get the assemble landuse map
-    lcover = ee.Image('projects/servir-mekong/Assemblage/MekongAssemblage_Mode_2015')
+    lcover = ee.Image('projects/servir-mekong/Assemblage/MekongAssemblage_MaxProb_Mode_2015')
 
-    #    '6f6f6f', #// unknown
-    #    'aec3d4', #// water
-    #	 '111149', #// mangrove
-    #    '387242', #// tree (mixed evergreen/deciduous)
-    #    'c3aa69', #// shrub
-    #    'cc0013', #// impervious surface
-    #    '8dc33b', #// crop
-    #    '30eb5b', #// tree (deciduous) 
-    #	'152106', #// tree (evergreen: mixed broadleaf/needleleaf)
-    #    'ff4ef8', #// tree (evergreen: needleleaf)
+	# var PALETTE = [
+	#    '6f6f6f', // unknown
+	#    'aec3d4', // water
+	#    '111149', // mangrove
+	#    '387242', // tree (other)
+	#    'f4a460', // grass
+	#    '800080', // shrub
+	#    'cc0013', // built-up
+	#    '8dc33b', // crop
+	#    'ffff00', // rice
+	#    'c3aa69', // plantation
+	#    '152106', // tree (deciduous)
+	#    '115420', // tree (evergreen)
+	#];
 
-    PALETTE_list = '6f6f6f,aec3d4,111149,387242,c3aa69,cc0013,8dc33b,30eb5b,152106,ff4ef8'
 
-    lc_mapid = lcover.getMapId({'min': 0, 'max': 9, 'palette': PALETTE_list}) #'6f6f6f, aec3d4, 111149, 247400, 247400, 247400, 55ff00, 55ff00, a9ff00, a9ff00, a9ff00, 006fff, ffff00, ff0000, ffff00, 74ffe0, e074ff, e074ff'})
+    PALETTE_list = '6f6f6f,aec3d4,111149,387242,f4a460,800080,cc0013,8dc33b,ffff00,c3aa69,152106,115420'
+
+    lc_mapid = lcover.getMapId({'min': 0, 'max': 11, 'palette': PALETTE_list}) #'6f6f6f, aec3d4, 111149, 247400, 247400, 247400, 55ff00, 55ff00, a9ff00, a9ff00, a9ff00, 006fff, ffff00, ff0000, ffff00, 74ffe0, e074ff, e074ff'})
     
 
     # These could be put directly into template.render, but it
@@ -71,7 +76,7 @@ class updateLandCover(webapp2.RequestHandler):
     mylegend = mylegend.encode('ascii','ignore').strip("[").strip("]").split(",")
   
     # load the landcover map
-    lcover= ee.Image('projects/servir-mekong/Assemblage/MekongAssemblage_Mode_2015')
+    lcover = ee.Image('projects/servir-mekong/Assemblage/MekongAssemblage_MaxProb_Mode_2015')
     
     # make a palette
     PALETTE_list = '6f6f6f,aec3d4,111149,387242,c3aa69,cc0013,8dc33b,30eb5b,152106,ff4ef8'
