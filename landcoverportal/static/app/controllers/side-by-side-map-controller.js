3e17abc65abf99a7e1f0b1eb8a82c1f4f50540a2
unknown <bionicbiplov45@gmail.com>
2018-08-23 16:54:14 +0700

    link to map swipe tool

--- landcoverportal/static/app/controllers/side-by-side-map-controller.js
+++ landcoverportal/static/app/controllers/side-by-side-map-controller.js
@@ -18,11 +18,10 @@
         // Base Layers\
         var mapbox = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
 			{
-                minZoom: 4,
-				attribution: "Map data &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, " +
+                attribution: "Map data &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, " +
 				"<a href='http://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, " +
-				"Imagery � <a href='http://mapbox.com'>Mapbox</a>", // jshint ignore:line
-				id: 'mapbox.light'
+                "Imagery � <a href='http://mapbox.com'>Mapbox</a>", // jshint ignore:line
+                id: 'mapbox.light'
 			}
         );
         var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
