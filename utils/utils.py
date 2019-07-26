34492dc3e83b6898b9a39da336ba563d32af7d82
unknown <bionicbiplov45@gmail.com>
2019-07-26 16:54:27 +0700

    bugfix

--- utils/utils.py
+++ utils/utils.py
@@ -53,7 +53,7 @@ def transfer_files_to_user_drive(temp_file_name, user_email, user_id, file_name,
 def get_client_ip(request):
 
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    logger.info(request.META)
+    logger.error(request.META)
     if x_forwarded_for:
         return x_forwarded_for.split(',')[0]
     else:
