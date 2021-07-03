660d225bf1fb6be30a84386d77cba71e50dd313f
Thannarot <thannarotk@gmail.com>
2021-07-03 12:08:34 +0700

    updated pull script

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -4,7 +4,6 @@ source /home/rlcms/landcover_env/bin/activate
 cd /home/rlcms/rlcms-v2
 git reset --hard HEAD
 git pull
-gulp build
 python manage.py collectstatic
 sudo systemctl restart supervisor 
 sudo service nginx restart
